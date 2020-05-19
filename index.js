2f923cc39aad724b497581baa6f70b44373432fd
Ammar <ammar.badwan95@gmail.com>
2020-05-20 01:41:41 +0300

    finished addition,deletion and listing of database

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const Input = require('./lib/input');
 const Note = require('./lib/notes');
 const result = new Input;
-if (result.payload || result.deleteId){
-  new Note(result);
-}
+// if (result.payload || result.deleteId || result.action === 'delete'){
+//   new Note(result);
+// }
+new Note(result);
 
 console.log(result);
